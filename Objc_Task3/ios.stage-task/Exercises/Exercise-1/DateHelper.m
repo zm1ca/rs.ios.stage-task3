edd587e22b402ca603649bd6099fb991d91224af
zm1ca <zmicier.fied@yandex.ru>
2021-05-21 11:25:25 +0300

    feat: add implementation for exercise 1

--- Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-1/DateHelper.m
@@ -5,24 +5,58 @@
 #pragma mark - First
 
 -(NSString *)monthNameBy:(NSUInteger)monthNumber {
-    return nil;
+    switch (monthNumber) {
+        case 1: return @"January";
+        case 2: return @"February";
+        case 3: return @"March";
+        case 4: return @"April";
+        case 5: return @"May";
+        case 6: return @"June";
+        case 7: return @"July";
+        case 8: return @"August";
+        case 9: return @"September";
+        case 10: return @"October";
+        case 11: return @"November";
+        case 12: return @"December";
+            
+        default: return nil;;
+    }
 }
 
 #pragma mark - Second
 
 - (long)dayFromDate:(NSString *)date {
-    return 0;
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    dateFormatter.dateFormat = @"yyyy-MM-dd'T'HH:mm:ss'Z'";
+    return [[NSCalendar currentCalendar] component:NSCalendarUnitDay fromDate:[dateFormatter dateFromString:date]];
 }
 
 #pragma mark - Third
 
 - (NSString *)getDayName:(NSDate*) date {
-    return nil;
+    switch ([[NSCalendar currentCalendar] component:NSCalendarUnitWeekday fromDate: date]) {
+        case 1: return @"Вс";
+        case 2: return @"Пн";
+        case 3: return @"Вт";
+        case 4: return @"Ср";
+        case 5: return @"Чт";
+        case 6: return @"Пт";
+        case 7: return @"Сб";
+        default: return nil;
+    }
 }
 
 #pragma mark - Fourth
 
 - (BOOL)isDateInThisWeek:(NSDate *)date {
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    NSDateComponents *currentDateComponents = [calendar components:(NSCalendarUnitWeekOfYear | NSCalendarUnitYear) fromDate:[NSDate now]];
+    NSDateComponents *givenDateComponents = [calendar components:(NSCalendarUnitWeekOfYear | NSCalendarUnitYear) fromDate:date];
+    
+    if (currentDateComponents.year == givenDateComponents.year) {
+        return currentDateComponents.weekOfYear == givenDateComponents.weekOfYear;
+    }
+    
     return NO;
 }
 
