11444540d1314b3235bd5ef813b5d8a26f793469
zm1ca <zmicier.fied@yandex.ru>
2021-05-21 11:25:31 +0300

    feat: add implementation for exercise 2

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -3,7 +3,18 @@
 @implementation PlayersSeparator
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
-    return 0;
+    int count = 0;
+    for (int i = 0; i < ratingArray.count; ++i) {
+        for (int j = i + 1; j < ratingArray.count; ++j) {
+            for (int k = j + 1; k < ratingArray.count; ++k) {
+                if (((([ratingArray[i] intValue] < [ratingArray[j] intValue]) && ([ratingArray[j] intValue]) < [ratingArray[k] intValue])) ||
+                ((([ratingArray[i] intValue] > [ratingArray[j] intValue]) && ([ratingArray[j] intValue] > [ratingArray[k] intValue])))){
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
 }
 
 @end
