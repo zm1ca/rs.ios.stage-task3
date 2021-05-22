ef8f19c765aa27b738b69fa6c0402f814c6b946b
zm1ca <zmicier.fied@yandex.ru>
2021-05-22 11:29:44 +0300

    feat: simplify implementation of exercise 3

--- Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-3/LexicographicallyMinimal.m
@@ -9,27 +9,22 @@
 @implementation LexicographicallyMinimal
 
 -(NSString *)findLexicographicallyMinimalForString1:(NSString *)string1 andString2:(NSString *)string2 {
+    NSMutableString *mutableString1 = [NSMutableString stringWithString:string1];
+    NSMutableString *mutableString2 = [NSMutableString stringWithString:string2];
     NSMutableString *result = [NSMutableString new];
-    int string1UsedCharacters = 0, string2UsedCharacters = 0;
-    NSString *tmp1 = [NSString new];
-    NSString *tmp2 = [NSString new];
     
-    while ((string1UsedCharacters < string1.length) && (string2UsedCharacters < string2.length)) {
-        tmp1 = [string1 substringWithRange:NSMakeRange(string1UsedCharacters, 1)];
-        tmp2 = [string2 substringWithRange:NSMakeRange(string2UsedCharacters, 1)];
+    while ((mutableString1.length != 0) && (mutableString2.length != 0)) {
+        [mutableString1 characterAtIndex:0];
         
-        if ([tmp1 UTF8String][0] <= [tmp2 UTF8String][0]) {
-            [result appendString:tmp1];
-            string1UsedCharacters++;
-        } else { //code duplication. consider blocks
-            [result appendString:tmp2];
-            string2UsedCharacters++;
+        if ([mutableString1 UTF8String][0] <= [mutableString2 UTF8String][0]) {
+            [result appendFormat:@"%c", [mutableString1 characterAtIndex:0]];
+            [mutableString1 deleteCharactersInRange:NSMakeRange(0, 1)];
+        } else {
+            [result appendFormat:@"%c", [mutableString2 characterAtIndex:0]];
+            [mutableString2 deleteCharactersInRange:NSMakeRange(0, 1)];
         }
     }
-    
-    string1UsedCharacters == string1.length
-        ? [result appendString:[string2 substringFromIndex:string2UsedCharacters]]
-        : [result appendString:[string1 substringFromIndex:string1UsedCharacters]];
+    mutableString1.length == 0 ? [result appendString:mutableString2] : [result appendString:mutableString1];
     
     return result;
 }
