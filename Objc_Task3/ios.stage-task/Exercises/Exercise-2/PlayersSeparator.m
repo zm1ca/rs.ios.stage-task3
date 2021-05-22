2edee57ea976b991024a3dd508c3c55ffed16add
zm1ca <zmicier.fied@yandex.ru>
2021-05-22 11:14:30 +0300

    feat: simplified exercise 2 implementation

--- Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
+++ Objc_Task3/ios.stage-task/Exercises/Exercise-2/PlayersSeparator.m
@@ -4,11 +4,21 @@
 
 - (NSInteger)dividePlayersIntoTeams:(NSArray<NSNumber *>*)ratingArray {
     int count = 0;
+    BOOL ratingIlessThanJ, ratingJLessThanK, sortedAscending, ratingIGreaterThanJ, ratingJGreaterThanK, sortedDescending;
+    
     for (int i = 0; i < ratingArray.count; ++i) {
         for (int j = i + 1; j < ratingArray.count; ++j) {
             for (int k = j + 1; k < ratingArray.count; ++k) {
-                if (((([ratingArray[i] intValue] < [ratingArray[j] intValue]) && ([ratingArray[j] intValue]) < [ratingArray[k] intValue])) ||
-                ((([ratingArray[i] intValue] > [ratingArray[j] intValue]) && ([ratingArray[j] intValue] > [ratingArray[k] intValue])))){
+                // MARK: Preparations
+                ratingIlessThanJ = [ratingArray[i] intValue] < [ratingArray[j] intValue];
+                ratingJLessThanK = [ratingArray[j] intValue] < [ratingArray[k] intValue];
+                sortedAscending = ratingIlessThanJ && ratingJLessThanK;
+                ratingIGreaterThanJ = [ratingArray[i] intValue] > [ratingArray[j] intValue];
+                ratingJGreaterThanK = [ratingArray[j] intValue] > [ratingArray[k] intValue];
+                sortedDescending = ratingIGreaterThanJ && ratingJGreaterThanK;
+                
+                // MARK: Checking condition
+                if (sortedAscending || sortedDescending){
                     count++;
                 }
             }
